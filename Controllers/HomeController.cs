00af53a2f94b16d969c14ffe78e70d792b4201cc
ibrahimsaleh1986 <37960053+ibrahimsaleh1986@users.noreply.github.com>
2020-03-15 15:33:59 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DockerTest.Models;
+using DockerTest.Models;
 
 namespace DockerTest.Controllers
 {
